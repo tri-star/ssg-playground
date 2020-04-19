49401e0d9acce1c1fe08b1a7edccc0f4d2e3fb62
tristar <tristar@urban-theory.net>
2020-04-19 11:04:41 +0000

    vuepress関連のページを追加

---
+++ vuepress/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: "VuePress Playground",
+  themeConfig: {
+    theme: '@vuepress/vue',
+    repo: 'tri-star/ssg-playground',
+    editLinks: true,
+    smoothScroll: true,
+    sidebar: {
+      '/': [
+        ['/', 'Top'],
+        '/markdown.md',
+        {
+          title: 'Vue',
+          children: [
+            ['/vue/', 'Top'],
+            './vue/install.md',
+            './vue/basic.md',
+            './vue/component.md',
+            './vue/transition.md',
+          ]
+        }
+      ]
+    }
+  }
+}
---
+++ vuepress/index.md
@@ -0,0 +1,12 @@
+# SSG Playground
+
+## コンテンツ
+* [Markdown記法](markdown.md)
+
+## サンプルページ(昔書いたもの)
+* [Vue.js](./vue/index.md)
+    * [インストール](./vue/install.md)
+    * [基本](./vue/basic.md)
+    * [コンポーネント](./vue/component.md)
+    * [Transition](./vue/transition.md)
+
